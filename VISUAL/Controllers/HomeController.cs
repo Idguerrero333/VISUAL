7bd30a3e471e9857935ab857e966889f0b14bf29
idg333 <idguerrero333@gmail.com>
2025-06-20 18:53:25 -0500

    feature pagina contactos creada

--- VISUAL/Controllers/HomeController.cs
+++ VISUAL/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace VISUAL.Controllers
             return View();
         }
 
+        public IActionResult contactos()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
